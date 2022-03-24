b532ba34e48dbd72ce8024209e9f62371a6cff3f
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-24 17:12:52 +0000

    5 files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Created by: Marshall
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area of a triangle.
+ */
+function calculate () {
+  // input
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const area = (base * height) / 2
+
+   // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
